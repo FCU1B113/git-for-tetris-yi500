c5420158c4ed7172ddd1d8bac2459492a4bdbdda
yi500 <wweut001@gmail.com>
2025-04-24 11:33:49 +0800

    加入專案檔案。

---
+++ TETR/main.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef enum {
+    RED = 41,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
+    BLACK = 0,
+}Color;
+
+typedef enum {
+    EMPTY = -1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+}ShapeId;
+
+
+typedef struct {
+    ShapeId shape;
+    Color color;
+    int size;
+    char rotates[4][4][4];
+}Shape;
+
+
+typedef struct {
+    Color color;
+    ShapeId shape;
+    bool current;
+}Block;
+
+Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+
+
+
+Shape{
+    .shape = I;
+    .color = WHITE;
+    .size = 4;
+    .rotates = {
+        {
+
+        }
+    }
+{
+    .shape = 0;
+    .color = WHITE;
+    .size = 2;
+}   .rotates = {
+        {
+            {1,1},
+            {1,1}
+        },
+        {
+            {1,1},
+            {1,1}
+        },{
+            {1,1},
+            {1,1}
+        },{
+            {1,1},
+            {1,1}
+        }
+    }
+    {
+        .shape = J;
+        .color = BLUE;
+        .size = 3;
+        .rotates = {
+            {
+
+            }
+        }
+    }
+    {
+        .shape = J;
+        .color = BLUE;
+        .size = 3;
+        .rotates = {
+            {
+
+            }
+        }
+    }
+    {
+        .shape = J;
+        .color = BLUE;
+        .size = 3;
+        .rotates = {
+            {
+
+            }
+        }
+    }
+    {
+        .shape = J;
+        .color = BLUE;
+        .size = 3;
+        .rotates = {
+            {
+
+            }
+        }
+    }
+
+
+
+}
+
+int main() {
+    Color cur;
+    //有幾種方塊
+    for (int i = 0; i < 7; i++) {
+        for (int r = 0; r < 4; r++) {
+            //印出方塊樣式
+            for (int s = 0; s < shape[i].size; s++) {
+                if (shape[i].rotates[r][s][t]) {
+                    cur = WHITE;
+                }
+                else {
+                    cur = shape[i].colar;
+                }
+            }
+
+        }
+    }
+}
